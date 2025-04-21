5e2b46549ca393f8ea52b842c4d2abae86e64b05
MWMasterWolf <almontetejedanelson@gmail.com>
2025-04-20 21:07:23 -0400

    wip: wip type relations

--- src/lib/components/type/TypeDefense.svelte
+++ src/lib/components/type/TypeDefense.svelte
@@ -6,31 +6,52 @@
 	import { Card, Heading } from 'flowbite-svelte';
 	import { doFetch } from '$lib/api/pokemon.api';
 	import { onMount } from 'svelte';
-	import type { Type } from 'pokeapi-typescript';
+	import type { Type, TypeRelations } from 'pokeapi-typescript';
 
 	let { info, profile }: { info: InfoSection; profile: PokemonProfile } = $props();
 
 	async function fetchTypeDamageRelations() {
-		const data = [];
+		// TODO: Separar el fetch de las operaciones con arreglos
+		const types: Type[] = [];
 		for (const type of profile.pokemon.types) {
-			data.push((await doFetch('type', type.type.name)) as Type);
+			types.push((await doFetch('type', type.type.name)) as Type);
 		}
 
-		const foo = data
-			// .map((value) => {
-			// 	return {
-			// 		double_damage_from: value.damage_relations.double_damage_from
-			// 		// half_damage_from: value.damage_relations.half_damage_from,
-			// 		// no_damage_from: value.damage_relations.no_damage_from
-			// 	};
-			// })
-			.flatMap((value) => {
-				return value.damage_relations.double_damage_from;
-			})
-			.map((value) => value.name);
-		console.log(foo);
+		const typeRelations: Record<string, Record<string, number>> = {
+			double_damage_from: {},
+			half_damage_from: {},
+			no_damage_from: {}
+		};
 
-		return data.map((value) => value);
+		const typeRelationsKeys = [
+			{
+				key: 'double_damage_from',
+				product: 2
+			},
+			{
+				key: 'half_damage_from',
+				product: 0.5
+			},
+			{
+				key: 'no_damage_from',
+				product: 0
+			}
+		];
+
+		for (const type of types) {
+			for (const relationKey of typeRelationsKeys) {
+				const currentRelationKey = relationKey.key as keyof TypeRelations;
+				for (const damageType of type.damage_relations[currentRelationKey]) {
+					const name = damageType.name;
+					typeRelations[currentRelationKey][name] =
+						(typeRelations[currentRelationKey][name] || 1) * relationKey.product;
+				}
+			}
+		}
+		console.log(typeRelations);
+		console.log(Object.entries(typeRelations).map(([type, values]) => ({ type, values: values })));
+
+		return Object.entries(typeRelations).map(([type, values]) => ({ type, values: values }));
 	}
 
 	onMount(async () => {
